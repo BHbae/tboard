dfcdc4684625cb008eed3f33c7c14bcdfd7276d0
BHbae <majinga15@naver.com>
2024-07-15 10:35:26 +0900

    first commit

---
+++ src/main/java/com/tenco/tboard/util/DBUtil.java
@@ -0,0 +1,32 @@
+package com.tenco.tboard.util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import org.eclipse.tags.shaded.org.apache.regexp.recompile;
+
+public class DBUtil {
+
+	private static DataSource dataSource;
+
+	static {
+
+		try {
+			InitialContext ctx = new InitialContext();
+			dataSource = (DataSource) ctx.lookup("java:comp/env/jdbc/tboard");
+		} catch (Exception e) {
+			System.out.println("DBUtil 초기화 실패");
+			e.printStackTrace();
+		}
+
+	}
+
+	public static Connection getConnection() throws SQLException {
+
+		return dataSource.getConnection();
+	}
+
+}
